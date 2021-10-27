acd48dbc9cada14d2c17ee52f94b724d7e8e9f47
Gabriel Torregrosa <g.torregrosa@outlook.com>
2021-10-27 15:20:17 +0200

    clean

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 var=0.1
 
 #Build arm64
-docker buildx build --platform linux/arm64 -t dsblab/single_cell_tutorial:$var-arm64 .
-docker push dsblab/single_cell_tutorial:$var-arm64
+# docker build -t dsblab/single_cell_tutorial:$var-arm64 .
+# docker push dsblab/single_cell_tutorial:$var-arm64
 
 #Build amd64
 # docker buildx build --platform linux/amd64 -t dsblab/single_cell_tutorial:$var-amd64 .
