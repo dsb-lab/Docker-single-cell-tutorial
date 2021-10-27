c290b965edcf138b104efc5a50b2423f3b5eb872
Gabriel Torregrosa <g.torregrosa@outlook.com>
2021-10-27 10:27:40 +0200

    Update README

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-var=0.2-light
+var=0.1
 
-# docker buildx build --platform linux/amd64 -t dsblab/single_cell_analysis:$var-amd64 .
-docker buildx build --platform linux/arm64 -t dsblab/single_cell_analysis:$var-arm64 .
+# docker buildx build --platform linux/amd64 -t dsblab/single_cell_tutorial:$var-amd64 .
+docker buildx build --platform linux/arm64 -t dsblab/single_cell_tutorial:$var-arm64 .
 
-# docker push dsblab/single_cell_analysis:$var-amd64
-# docker push dsblab/single_cell_analysis:$var-arm64
+# docker push dsblab/single_cell_tutorial:$var-amd64
+# docker push dsblab/single_cell_tutorial:$var-arm64
 
-# docker manifest create amouat/single_cell_analysis:$var dsblab/single_cell_analysis:$var-amd64 dsblab/single_cell_analysis:$var-arm64
-# docker push dsblab/single_cell_analysis:$var
+# docker manifest create amouat/single_cell_tutorial:$var dsblab/single_cell_tutorial:$var-amd64 dsblab/single_cell_tutorial:$var-arm64
+# docker push dsblab/single_cell_tutorial:$var
